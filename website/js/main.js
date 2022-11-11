f4eabf2444925aa07138b1a603f4a3664e492c49
punalex <mailbox@punalex.net>
2022-11-11 17:07:33 +0800

    Add files via upload

---
+++ website/js/main.js
@@ -0,0 +1,40 @@
+var timetag="20221111161801";
+function LoadHash(){
+	var xmlhttp, langcbutton, langebutton;
+	
+	clickid = (window.location.href.indexOf('#') != -1 && window.location.href.indexOf('!') != -1 && window.location.href.split("!")[1] != '')?(window.location.href.split("!")[1]):('main');
+	if (window.XMLHttpRequest)
+		xmlhttp = new XMLHttpRequest();
+	else
+		xmlhttp = new window.ActiveXObject("Microsoft.XMLHTTP");	
+		
+	if (clickid != ''){
+		xmlhttp.onreadystatechange = function(){
+			alert(xmlhttp.status);
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+				alert("test"+xmlhttp.responseText+"))");
+				replacehtml(xmlhttp.responseText);
+//				Menu_Out();
+//				Resize((size == '-l')?(3):((size == '-s')?(-3):(0)));
+			}
+		}
+		if (clickid.substr(0, 4) == 'main'){
+			$(document).prop('title', '待定');
+			xmlhttp.open('GET', 'content/main.html?'+timetag, true);
+		}
+		else if (clickid.substr(0, 7) == 'aboutus'){
+			xmlhttp.open('GET', 'content/aboutus.html?time='+timetag, true);
+		}
+		xmlhttp.send();
+	}
+}
+$(window).on('hashchange', function(){
+	LoadHash();
+})
+function replacehtml(text){
+	$('#main').html(text);
+	$('html,body').animate({scrollTop:0}, 'slow');
+}
+$(document).ready(function(){
+	LoadHash();
+})
