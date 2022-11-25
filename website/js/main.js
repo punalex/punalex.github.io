e2901f4eb049e6e004f842be6d75c3c72a378002
punalex <mailbox@punalex.net>
2022-11-25 11:15:10 +0800

    Add files via upload

--- website/js/main.js
+++ website/js/main.js
@@ -1,9 +1,14 @@
-var timetag="20221111161801";
+var timetag="20221111161803";
 var audio=new Audio;
+var timeout=0;
+var windowresize=0;
 function LoadHash(){
-	var xmlhttp, langcbutton, langebutton;
+	var xmlhttp, langcbutton, langebutton, type;
 	
-	clickid = (window.location.href.indexOf('#') != -1 && window.location.href.indexOf('!') != -1 && window.location.href.split("!")[1] != '')?(window.location.href.split("!")[1]):('main');
+	clickid = (window.location.href.indexOf('#') != -1 && window.location.href.indexOf('!') != -1 && window.location.href.split("!")[1] != '')?(window.location.href.split("!")[1]):('main');	
+	type = sessionStorage.getItem('type');
+	if (type == null || clickid.substr(0, 4) == 'main')
+		type = '1';
 	if (window.XMLHttpRequest)
 		xmlhttp = new XMLHttpRequest();
 	else
@@ -17,36 +22,45 @@ function LoadHash(){
 //				Resize((size == '-l')?(3):((size == '-s')?(-3):(0)));
 			}
 		}
+		var bk;
+		switch(type){
+			case '1':{
+				bk = 'bg_orange.png';
+			}break;
+			case '2':{
+				bk = 'bg_green.png';
+			}break;
+		}				
+		if ($('body').css('background-image').indexOf(bk) == -1)
+			$('body').css('background-image', 'url("images/'+bk+'")');
 		if (clickid.substr(0, 4) == 'main'){
 			xmlhttp.open('GET', 'content/main.html?'+timetag, true);
-			$('#buttonbox').css('visibility', 'hidden');
+			$('body > div:not(#main)').each(function(){
+				$(this).css('visibility', 'hidden');
+			})
 		}
 		else{
-			$('#info').attr('src', 'images/btn_elders_info_on.png');
-			$('#support').attr('src', 'images/btn_elders_support_on.png');
-			$('#member').attr('src', 'images/btn_elders_members_on.png');
-			$('#timetable').attr('src', 'images/btn_elders_timetable_on.png');
-			$('#buttonbox').css('visibility', 'visible');
-			if (clickid.substr(0, 5) == 'login'){
-				$('#info').attr('src', 'images/btn_elders_info.png');
-				xmlhttp.open('GET', 'content/info.html?'+timetag, true);
-			}
-			else if (clickid.substr(0, 4) == 'info'){
-				$('#info').attr('src', 'images/btn_elders_info.png');
-				xmlhttp.open('GET', 'content/info.html?'+timetag, true);
+			$('body > div:not(#main)').each(function(){
+				$(this).css('visibility', ($(this).attr('id').substr(-1) == type)?('visible'):('hidden'));
+			})
+			$('#buttonbox'+type+' > img').each(function(){
+				if ($(this).attr('src').indexOf('on.png') == -1)
+					$(this).attr('src', $(this).attr('src').substring(0, $(this).attr('src').length-4)+'_on.png');
+			})
+			$('#'+clickid).attr('src', $('#'+clickid).attr('src').substring(0, $('#'+clickid).attr('src').length-7)+'.png');
+			xmlhttp.open('GET', 'content/'+clickid+'.html?'+timetag, true);
+/*			if (clickid.substr(0, 4) == 'info'){
+				xmlhttp.open('GET', 'content/'+clickid+'.html?'+timetag, true);
 			}				
-			else if (clickid.substr(0, 7) == 'support'){
-				$('#support').attr('src', 'images/btn_elders_support.png');
-				xmlhttp.open('GET', 'content/support.html?'+timetag, true);
+			else if (clickid.substr(0, 8) == 'support1'){
+				xmlhttp.open('GET', 'content/support1.html?'+timetag, true);
 			}				
-			else if (clickid.substr(0, 6) == 'member'){
-				$('#member').attr('src', 'images/btn_elders_members.png');
-				xmlhttp.open('GET', 'content/member.html?'+timetag, true);
+			else if (clickid.substr(0, 7) == 'member1'){
+				xmlhttp.open('GET', 'content/member1.html?'+timetag, true);
 			}				
-			else if (clickid.substr(0, 9) == 'timetable'){
-				$('#timetable').attr('src', 'images/btn_elders_timetable.png');
-				xmlhttp.open('GET', 'content/timetable.html?'+timetag, true);
-			}
+			else if (clickid.substr(0, 10) == 'timetable1'){
+				xmlhttp.open('GET', 'content/timetable1.html?'+timetag, true);
+			}*/
 		}
 		xmlhttp.send();
 	}
@@ -56,7 +70,43 @@ $(window).on('hashchange', function(){
 })
 function OnClick() {
 	clickid = (this.id != undefined)?(this.id):((window.location.href.indexOf('#') != -1 && window.location.href.indexOf('!') != -1 && window.location.href.split("!")[1] != '')?(window.location.href.split("!")[1]):('main'));
-	if (clickid.substr(0, 5) == 'close'){
+	if (clickid.substr(0, 5) == 'login'){
+		var xmlhttp, langcbutton, langebutton;
+	
+		if (window.XMLHttpRequest)
+			xmlhttp = new XMLHttpRequest();
+		else
+			xmlhttp = new window.ActiveXObject("Microsoft.XMLHTTP");	
+
+		xmlhttp.onreadystatechange = function(){
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+				var type=xmlhttp.responseText;		
+				// for testing
+				type = $('#id').val();
+				if (type != '1' && type != '2')
+					type = 'error';
+				//
+				if (!Number.isNaN(Number(type)) && Number(type) > 0 && Number(type) < 3){		
+					sessionStorage.setItem('type', type);
+					location.hash = '#!info'+type;
+				}
+				else
+					$('#logindesc').css('display', 'none');
+					$('#error').css('display', 'block');
+					clearTimeout(timeout);
+					timeout = setTimeout(function (){
+						$('#logindesc').css('display', 'block');
+						$('#error').css('display', 'none');						
+						clearTimeout(timeout);
+					}, 5000);
+//				Menu_Out();
+//				Resize((size == '-l')?(3):((size == '-s')?(-3):(0)));
+			}
+		}
+		xmlhttp.open('GET', 'login.txt?'+timetag, true);
+		xmlhttp.send();
+	}
+	else if (clickid.substr(0, 5) == 'close'){
 		if ($('#'+clickid.substr(0, 6)).html().indexOf('down') != -1){
 			$('#'+clickid.substr(0, 6)).html('收起 <span class="fa fa-chevron-up">');
 			$('#e'+clickid.substr(5, 1)+'d').css('display', 'block');
@@ -89,10 +139,12 @@ function replacehtml(text){
 	$('#login').unbind('click');
 	$('#howlogin').unbind('click');
 	$('#howgroup').unbind('click');
-	$('#info').unbind('click');
-	$('#support').unbind('click');
-	$('#member').unbind('click');
-	$('#timetable').unbind('click');
+	for (let loop=1; loop<=2; loop++){
+		$('#buttonbox'+loop+' > img').each(function(){
+			$(this).unbind('click');
+			$(this).click(OnClick);
+		})
+	}
 	$('#infoedit').unbind('click');	
 	$('#groupsong').unbind('click');
 	$('#close1').unbind('click');
@@ -103,10 +155,6 @@ function replacehtml(text){
 	$('#login').click(OnClick);
 	$('#howlogin').click(OnClick);
 	$('#howgroup').click(OnClick);
-	$('#info').click(OnClick);
-	$('#support').click(OnClick);
-	$('#member').click(OnClick);
-	$('#timetable').click(OnClick);
 	$('#infoedit').click(OnClick);
 //	$('#groupsong').click(OnClick);
 	$('#close1').click(OnClick);
@@ -133,7 +181,13 @@ $(document).ready(function(){
 	LoadHash();
 })
 $(window).resize('resize', function(){
-	let vh = window.innerHeight * 0.01;
+	clearTimeout(windowresize);
+	windowresize = setTimeout(function (){
+		let vh = window.innerHeight * 0.01;	
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+		clearTimeout(windowresize);
+	}, 500);
+	let vh = window.innerHeight * 0.01;	
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 })
 $(window).on('orientationchange', function(){
